e0493d8364bc1e0dbf2d030d0563be23972ff86f
antsteyer <antoinesteyer@gmail.com>
2019-01-28 16:25:59 +0100

    Add map component

---
+++ src/components/StoresMap.vue
@@ -0,0 +1,22 @@
+<template>
+  <GmapMap
+    :center="{lat:10, lng:10}"
+    :zoom="7"
+    map-type-id="terrain"
+    style="width: 500px; height: 500px"
+  >
+    <!-- <GmapMarker
+    :key="index"
+    v-for="(m, index) in markers"
+    :position="m.position"
+    :clickable="true"
+    :draggable="true"
+    @click="center=m.position"
+    />-->
+  </GmapMap>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
