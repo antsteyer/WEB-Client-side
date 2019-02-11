c3d88ad3b99e95210d1bf9f655f066c3f0fae4ad
antsteyer <antoinesteyer@gmail.com>
2019-02-11 15:30:04 +0100

    Update RecipeDetails when id changed in route

--- src/router.ts
+++ src/router.ts
@@ -29,7 +29,7 @@ export default new Router({
       component: Recipe,
       children: [
         {
-          path: "details",
+          path: "details/:id",
           name: "recipedetails",
           component: RecipeDetails
         },
